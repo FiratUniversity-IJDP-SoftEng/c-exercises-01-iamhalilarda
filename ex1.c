ee4131fcd899afa32b19b5288d06951b5d9b1b88
iamhalilarda <111307505+iamhalilarda@users.noreply.github.com>
2025-10-26 18:20:52 +0300

    Complete assignment

--- ex1.c
+++ ex1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int sayi = 3;   // Bilgisayarın tuttuğu sayı
+    int tahmin;     // Kullanıcının tahmini
+
+    printf("1 ile 5 arasinda bir sayi tahmin et: ");
+    scanf("%d", &tahmin);
+
+    if (tahmin == sayi) {
+        printf("Dogru bildin! \n");
+    } else {
+        printf("Yanlis bildin! Sayi %d idi.\n", sayi);
+    }
+
+    return 0;
+}
